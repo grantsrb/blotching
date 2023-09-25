4aca6133bdde1c139f7c3cf40f6e97c7e0cfae7a
Satchel Grant <grantsrb@stanford.edu>
2023-09-25 06:19:12 -0700

    fix solution mask function, add equal spacing to blotch function

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import math
 
 DEVICES = {
     **{-1: "cpu"},
@@ -120,10 +121,13 @@ def get_soln_mask(
         eos_id,
         sep_id,
         pad_id,
-        has_conf=False
+        has_conf=False,
+        incl_eos=False,
+        max_len=None,
     ):
     """
-    Finds a boolean mask over the final answer, no sep or eos included
+    Finds a boolean mask over the final answer, no sep or eos included.
+    eos can be included if incl_eos is true.
 
     # We find the eos in solutions
     # We then find the ground truth solution length
@@ -136,36 +140,51 @@ def get_soln_mask(
         sep_id: int
         pad_id: int
         has_conf: bool
-            if true, will assume a conf prediction follows the sep
+            if true, will assume a confidence prediction follows the sep
             token
+        incl_eos: bool
+            if true, will include the eos token in the mask
+        max_len: int or None
+            the maximum length response. if None, no maximum
     Returns:
         ans_mask: Bool Tensor (N,)
-            True values indicate the final output
+            True values indicate the final output. excludes the separator.
+            includes the eos if incl_eos is true.
     """
+    if max_len is None: max_len = ids.shape[-1]
     device = DEVICES[ids.get_device()]
+    ids = ids.clone()
+    ids[:,-1] = eos_id
 
-    seps = ids==sep_id
     aranges = torch.arange(ids.shape[1])[None].repeat(
         (len(ids), 1)
     ).to(device)
 
-    soln_ends = torch.argmax( (ids==eos_id).long(),dim=-1 )
+    soln_ends = torch.argmax( (ids==eos_id).long(),dim=-1 )[:,None]
+    ids[aranges>soln_ends] = pad_id
 
     # Need to look at padding because it's possible to have
     # =PPPP45E as an answer. Padding is not to be worried about later
     # because it's factored into the sum to find the last index
-    seps = (seps|((aranges<soln_ends[:,None])&(ids==pad_id))).long()
+    seps = ids==sep_id
+    seps = (seps|((aranges<soln_ends)&(ids==pad_id)))
 
     last_sep_idxs = arglast(seps.long(), dim=-1)+int(has_conf)
-    soln_ends = soln_ends[:,None]
-    soln_lens = soln_ends - last_sep_idxs[:,None]
-    soln_idxs = (aranges<soln_ends)&(aranges>=(soln_ends-soln_lens))
+    # Subtract 1 to exclude the separator
+    soln_lens = soln_ends - last_sep_idxs[:,None] - 1
+    soln_lens[soln_lens>=max_len] = max_len-int(incl_eos)
+    if incl_eos:
+        soln_idxs = (aranges<=soln_ends)&(aranges>=(soln_ends-soln_lens))
+    else:
+        soln_idxs = (aranges<soln_ends)&(aranges>=(soln_ends-soln_lens))
     return soln_idxs
 
 def arglast(mask, dim=-1):
     """
     This function finds the index of the last max value along a given
-    dimension.
+    dimension. torch.flip creates a copy of the tensor, so it's
+    actually not as efficient as using numpy's np.flip which only
+    returns a view.
 
     Args:
         mask: bool (B,N)
@@ -176,6 +195,24 @@ def arglast(mask, dim=-1):
     argmaxs = torch.argmax(torch.flip(mask, dims=(dim,)), dim=dim)
     return mask.shape[dim] - argmaxs - 1
 
+def mask_up_to_idx(idxs, M, from_left=True):
+    """
+    Args:
+        idxs: torch long tensor (B,)
+        from_left: bool
+    Returns:
+        mask: torch bool tensor (B, M)
+    """
+    B = idxs.shape[0]
+    device = idxs.get_device()
+    if device<0: device = "cpu"
+    arange = torch.arange(M).to(device)[None].repeat((B,1))
+    if from_left:
+        mask = arange<idxs[:,None]
+    else:
+        mask = arange>=idxs[:,None]
+    return mask
+
 def get_blotch_mask(
         idxs,
         sep_idx,
@@ -183,6 +220,7 @@ def get_blotch_mask(
         allow_contig=True,
         indy_blotching=False,
         step_idx=None,
+        spacing="random",
     ):
     """
     Blotches out entire segments of the input based on the sep_idx. For
@@ -222,8 +260,32 @@ def get_blotch_mask(
             if int is argued, will only drop the argued semantic step.
             An argument of 0 refers the first semantic step following
             the initial problem. This function will not blotch the
-            final answer. The final answer is defined by the content
-            after the last sep token and including the eos token.
+            final answer or the initial problem. The final answer is
+            defined by the content after the last sep token and
+            including the eos token.
+        spacing: str
+            an argument to decide how the blotching will be spaced.
+            possible arguments are the following:
+                "random": blotching is random according to the blotching
+                    probability
+                "equal": the blotching is semi-deterministicly
+                    distributed relatively equally amongst the possible
+                    segments.  The number of blotched segments is decided
+                    by `blotch_p * <num possible segments>` where the
+                    rounding direction is decided randomly using a
+                    probability equal to the remaining fraction. So,
+                    there's still some stochasticity but it's lower
+                    variance and deterministic in many cases.
+
+                TODO (DOES NOT EXIST):
+                  "random_equal": the blotching is mostly spaced equally
+                    except that each equally spaced blotch segment has
+                    an equal probability of being located at the segment
+                    before or after the equally spaced segement. i.e.
+                    there is 2/3 probability that instead of blotching
+                    the segment chosen by equal spacing, we instead
+                    blotch the segment immediately before or after with
+                    equal probability.
     Returns:
         blotch_mask: torch BoolTensor (B,S)
             the shape will depend on the argument for indy_blotching
@@ -263,7 +325,14 @@ def get_blotch_mask(
         if type(blotch_p)==type(float()) or type(blotch_p)==type(int()):
             blotch_p = torch.full((1,1), blotch_p)
         elif len(blotch_p.shape)==1: blotch_p = blotch_p[..., None].cpu()
-        do_blotches = torch.rand(idxs.shape)<=blotch_p
+
+        if spacing=="random": 
+            do_blotches = torch.rand(idxs.shape)<=blotch_p
+        else:
+            do_blotches = get_equal_spaced_do_blotches(
+                idxs, blotch_p, seps
+            )
+
         is_contig = False
         for i in range(len(sep_coords)-1):
             # Check to blotch and both sep_coords are on same row
@@ -276,6 +345,55 @@ def get_blotch_mask(
             else: is_contig = False
     return mask
 
+def get_equal_spaced_do_blotches(idxs, blotch_p, seps):
+    # Complicated indexing to evenly space out the blotching steps
+    # The indexing first calculates the number of blotches based
+    # on the total possible and the argued proportion, blotch_p.
+    # Then it determines the number of indices to skip for each
+    # blotching segment. Lastly, it indexes into the ordered
+    # separator indices to mark which segments should be blotched.
+    device = idxs.get_device()
+    if device < 0: device = "cpu"
+    n_poss_blotches = seps.float().sum(-1) - 1
+    n_blotch = blotch_p.squeeze().to(device)*n_poss_blotches
+    n_blotches = torch.round(n_blotch).float()
+
+    # Randomly add another blotch with probability equal to the
+    # remainder
+    #flr = torch.floor(n_blotch)
+    #extra = ((n_blotch-flr)>torch.rand_like(n_blotch)).long()
+    #extra = torch.round((n_blotch-flr))
+    #n_blotches = flr + extra
+    #n_blotches = n_blotches.float()
+
+    # Find edges of bins in terms of indexes into the separators
+    # Index steps which will be used to index into the sep
+    # indices
+    idx_space = n_poss_blotches/n_blotches
+    inf_idx = n_blotches<=0 # only care about ==0, <= is better for floats
+    idx_space[inf_idx] = -1
+    m = int(torch.max(n_blotches))
+    steps = [ idx_space*i for i in range(m-1) ]
+    steps = torch.vstack(steps).T
+    offsets = (n_poss_blotches - idx_space*(n_blotches-1))/2
+    steps = torch.round(steps + torch.round(offsets[:,None])).long()
+    steps = steps + inf_idx[:,None].long()*-seps.shape[-1]
+
+    # Arguments where separators come ordered first
+    args = torch.arange(seps.shape[-1]).repeat((seps.shape[0],1))
+    args[~seps] = seps.shape[-1]
+    args = torch.argsort(args,dim=-1).to(device)
+    do_blotches = torch.zeros_like(idxs).bool()
+
+    # Will index into do_blotches at sep locations and set
+    # to 1 in cases 
+    row_idx = torch.arange(len(do_blotches)).long().to(device)
+    for s in range(steps.shape[1]):
+        select = steps[:,s]>=0
+        rows = row_idx[select]
+        step = steps[select,s]
+        do_blotches[rows, args[rows, step]] = True
+    return do_blotches
 
 def get_tok_mask(src, tok_p, sep_idx):
     """
@@ -390,13 +508,68 @@ def reverse_excision(logits, cur_mask, old_mask):
     og_logits[~old_mask.bool()] = logits[~cur_mask.bool()]
     return og_logits
 
-if __name__=="__main__":
-    mask = torch.randint(0,2, (2,5))
-    src = torch.arange(5,0, -1)[None].repeat((2,1))
-    print("Src   :", src)
-    print("Mask  :", mask)
-    contig,new_mask = excise_tokens(src, ~mask.bool(), pad_left=True)
+def get_one_hot(ids, L):
+    """
+    Args:
+        ids: torch long tensor (..., N)
+        L: int
+            the length of the one-hot vector
+    Returns:
+        one_hots: torch long tensor (..., N, L)
+    """
+    shape = [*ids.shape, L]
+    device = ids.get_device()
+    if device<0: device = "cpu"
+    one_hots = torch.zeros( shape, device=device )
+    one_hots.scatter_(
+        dim=-1,
+        index=ids[...,None],
+        src=torch.ones_like(one_hots)
+    )
+    return one_hots
 
-    print("Contig:", contig)
-    rev = reverse_excision(contig, new_mask, mask.bool())
-    print("Revers:", rev)
+def print_tensor(tensor):
+    s = ""
+    for row in range(tensor.shape[0]):
+         s += "\t" + str( tensor[row].tolist() ) + "\n"
+    return s
+
+def int_linspace(low, high, n_steps):
+    """
+    Want a list of integers that bin the distance from low to high
+    into `n_steps` equal sized bins. This function returns a list
+    of length `n_steps-1` with integers denoting the edges of each
+    bin.
+    """
+    diff = high-low
+    space = diff/n_steps
+    edges = torch.FloatTensor([space*i for i in range(n_steps-1)])
+    offset = (diff - edges[-1])/2
+    return (edges + offset).long()
+
+if __name__=="__main__":
+    print("linspace:", int_linspace(0,5,3))
+    sep_id = -1
+    seq_len = 25
+    n_rows = 5
+    blotch_p = torch.FloatTensor([0.5 for _ in range(n_rows)])
+    #blotch_p = 0.7
+    src = torch.arange(seq_len)[None].repeat((n_rows,1))
+    for i in range(len(src)):
+        src[i,i::i+2] = sep_id
+    print("Blotch P:", blotch_p)
+    print("Src:", print_tensor(src))
+    #print("SepCounts:", (src==sep_id).long().sum(-1))
+    mask = get_blotch_mask(src, sep_idx=sep_id, blotch_p=blotch_p, spacing="equal")
+    seps = src==sep_id
+    print("Seps:", print_tensor((seps).long()))
+    print("Mask:", print_tensor(mask.long()))
+    mask = mask.long()
+    mask[seps] = 5
+    print("SMaks:", print_tensor(mask.long()))
+    #print("Mask  :", mask)
+    #contig,new_mask = excise_tokens(src, ~mask.bool(), pad_left=True)
+
+    #print("Contig:", contig)
+    #rev = reverse_excision(contig, new_mask, mask.bool())
+    #print("Revers:", rev)
